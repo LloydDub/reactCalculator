df27edfaab89e2ab0471726acb5cb3e7308cf5c5
LloydDub <lloydrww@gmail.com>
2022-03-20 15:06:13 -0600

    started building hook with reducer

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,28 @@
-import logo from "./logo.svg";
 import "./styles.css";
 
+const ACTIONS = {
+  ADD_DIGIT: "add_digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand,
+      };
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
   return (
     <div className="calculator-grid">
       <div className="output">
@@ -18,7 +39,7 @@ function App() {
       <button>4</button>
       <button>5</button>
       <button>6</button>
-      <button>+</button>
+      <button>\</button>
       <button>7</button>
       <button>8</button>
       <button>9</button>
